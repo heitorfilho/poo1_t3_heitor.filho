3f4eb7a3ca3a1b2388a10febad044e750259404a
IgorAugust0 <heitor.filho@ufu.br>
2022-10-17 20:06:26 -0300

    teste

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+        System.out.println("Heitor Guimaraes da Fonseca Filho");
+        System.out.println("12011bsi203");
+    }
+
+}
+
