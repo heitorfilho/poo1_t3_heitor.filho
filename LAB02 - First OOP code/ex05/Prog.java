6606ff21369e929b1d201885d43115992a5c3211
heitorfilho <80076544+heitorfilho@users.noreply.github.com>
2022-11-16 18:20:45 -0300

    Add files via upload

---
+++ LAB02 - First OOP code/ex05/Prog.java
@@ -0,0 +1,13 @@
+package ex05;
+
+public class Prog {
+
+    public static void main(String[] args){
+
+        Selecao selecao1 = new Selecao("Brasil", "Tite", "Brasil");
+
+        selecao1.print();
+
+    }
+    
+}
---
+++ LAB02 - First OOP code/ex05/Selecao.java
@@ -0,0 +1,47 @@
+package ex05;
+
+public class Selecao {
+
+    String nomeSelecao;
+    String tecnico;
+    String pais;
+
+    Figurinha jogador[] = new Figurinha[11];
+
+    public Selecao(String nomeSelecao, String tecnico, String pais){
+
+        this.nomeSelecao = nomeSelecao;
+        this.tecnico = tecnico;
+        this.pais = pais;
+
+        for (int i = 0; i < jogador.length; i++) {
+            
+            jogador[i] = new Figurinha(pais);
+
+        }
+
+    }
+
+    
+    public void setJogador(Figurinha jogador, int i){
+
+        this.jogador[i] = new Figurinha(jogador);
+
+    }
+
+    public Figurinha getJogador(int i){
+
+        return this.jogador[i];
+    }
+
+    public void print() {
+        System.out.println("Nome da seleção: " + this.nomeSelecao);
+        System.out.println("Técnico: " + this.tecnico);
+        System.out.println("País: " + this.pais);
+        System.out.println("Jogadores: " + this.jogador);
+        for (int i = 0; i < jogador.length; i++) {
+            jogador[i].print();
+        }
+    }
+
+}
